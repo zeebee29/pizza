c23aae17bbab9c914269b6dd12ed93aeaf52c8e8
zeebee29 <coucoufd@gmail.com>
2022-07-13 22:51:04 +0200

    Add my new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'my pizza': {
+        'ingredients': {'mozzarella': 3, 'garlic': 5},
+        'price': 10
     }
 }
